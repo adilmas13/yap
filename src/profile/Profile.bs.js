de40047b57600c47849ab6cc711fc17b3a4aaa11
Adil Shaikh <adil@invideo.io>
2020-12-26 16:11:45 +0530

    feat: my name is animation

--- src/profile/Profile.bs.js
+++ src/profile/Profile.bs.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
 var Ru$Yap = require("../utils/ru.bs.js");
+var Belt_Array = require("bs-platform/lib/js/belt_Array.js");
 
 var parent = {
   display: "flex"
@@ -23,10 +25,22 @@ var description = {
   fontWeight: "600"
 };
 
+var rightParent = {
+  display: "flex",
+  paddingLeft: "20px",
+  alignItems: "center"
+};
+
+var text1 = {
+  fontSize: "70px"
+};
+
 var Style = {
   parent: parent,
   leftParent: leftParent,
-  description: description
+  description: description,
+  rightParent: rightParent,
+  text1: text1
 };
 
 function Profile$LeftSection(Props) {
@@ -41,15 +55,43 @@ var LeftSection = {
   make: Profile$LeftSection
 };
 
+function Profile$RightSection(Props) {
+  var match = React.useState(function () {
+        return "";
+      });
+  var setTitle = match[1];
+  React.useEffect((function () {
+          Belt_Array.forEachWithIndex("my name is".split(""), (function (index, $$char) {
+                  setTimeout((function (param) {
+                          return Curry._1(setTitle, (function (current) {
+                                        return current.concat($$char);
+                                      }));
+                        }), Math.imul(index + 1 | 0, 70));
+                  
+                }));
+          
+        }), []);
+  return React.createElement("div", {
+              style: rightParent
+            }, React.createElement("div", {
+                  style: text1
+                }, Ru$Yap.s(match[0])));
+}
+
+var RightSection = {
+  make: Profile$RightSection
+};
+
 function Profile(Props) {
   return React.createElement("div", {
               style: parent
-            }, React.createElement(Profile$LeftSection, {}));
+            }, React.createElement(Profile$LeftSection, {}), React.createElement(Profile$RightSection, {}));
 }
 
 var make = Profile;
 
 exports.Style = Style;
 exports.LeftSection = LeftSection;
+exports.RightSection = RightSection;
 exports.make = make;
 /* react Not a pure module */
--- src/profile/Profile.res
+++ src/profile/Profile.res
@@ -11,25 +11,46 @@ module Style = {
     ~alignItems="center",
     (),
   )
-  let description = make(~fontSize="40px", ~color="white", ~fontWeight="600", ());
+  let description = make(~fontSize="40px", ~color="white", ~fontWeight="600", ())
+
+  let rightParent = make(~display="flex", ~alignItems="center", ~paddingLeft="20px", ())
+
+  let text1 = make(~fontSize="70px", ())
 }
 
 module LeftSection = {
   @react.component
   let make = () => {
     open Style
-    <div style={leftParent}>
-      <div style={description}>
-      {"Let's talk" -> Ru.s}
-      </div>
-    </div>
+    <div style={leftParent}> <div style={description}> {"Let's talk"->Ru.s} </div> </div>
+  }
+}
+
+module RightSection = {
+  @react.component
+  let make = () => {
+    let (title, setTitle) = React.useState(() => "")
+
+    React.useEffect1(() => {
+      let text = "my name is"
+
+      text
+      ->Js.String2.split("")
+      ->Belt.Array.forEachWithIndex((index, char) => {
+        Js.Global.setTimeout(() => {
+          setTitle(current => Js.String2.concat(current, char))
+        }, (index + 1) * 70)->ignore
+      })
+
+      None
+    }, [])
+    open Style
+    <div style={rightParent}> <div style={text1}> {title->Ru.s} </div> </div>
   }
 }
 
 @react.component
 let make = () => {
   open Style
-  <div style={parent}>
-    <LeftSection/>
-  </div>
+  <div style={parent}> <LeftSection /> <RightSection /> </div>
 }
