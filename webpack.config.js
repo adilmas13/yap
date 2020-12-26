d88d118b5df708167977f47dda046d13d1d209c9
Adil Shaikh <adil@invideo.io>
2020-12-27 03:37:01 +0530

    feat: production build config

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,45 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const outputDir = path.join(__dirname, "dist")
 
-module.exports = {
-  entry: './src/Index.bs.js',
-  // If you ever want to use webpack during development, change 'production'
-  // to 'development' as per webpack documentation. Again, you don't have to
-  // use webpack or any other bundler during development! Recheck README if
-  // you didn't know this
-  mode: 'development',
-  output: {
-    path: outputDir,
-    filename: 'index.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      inject: false,
-    })
-  ],
-  devServer: {
-    compress: true,
-    contentBase: outputDir,
-    port: process.env.PORT || 8000,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jpg|png|svg)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'assets/'
-          }
-        },
-      }
+module.exports = env => {
+  console.log("ENVIRONMENT => ", env);
+  return {
+
+    entry: './src/Index.bs.js',
+    // If you ever want to use webpack during development, change 'production'
+    // to 'development' as per webpack documentation. Again, you don't have to
+    // use webpack or any other bundler during development! Recheck README if
+    // you didn't know this
+    mode: env.production ? 'production' : 'development',
+    output: {
+      path: outputDir,
+      filename: 'index.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        inject: false,
+      })
     ],
+    devServer: {
+      compress: true,
+      contentBase: outputDir,
+      port: process.env.PORT || 8000,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jpg|png|svg)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/'
+            }
+          },
+        }
+      ],
+    }
   }
 };
